dd6664895eb0a61df7bfa11612e09476c950d1a6
caisentialofficial-stack <caisentialofficial@gmail.com>
2026-05-14 19:04:46 +0800

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'staff-scanner-v1';
+const urlsToCache = [
+  '/staff-scanner/',
+  '/staff-scanner/index.html',
+  'https://unpkg.com/html5-qrcode@2.3.8/html5-qrcode.min.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
